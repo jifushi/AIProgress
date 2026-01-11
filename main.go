667e905a8157be9f6350185ece504d92784311b4
gaoxiaopeng <gaoxiaopeng@qq.com>
2026-01-11 19:01:23 +0800

    启动函数

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"AiProgress/common/mysql"
+	"AiProgress/common/rabbitmq"
+	"AiProgress/common/redis"
+	"AiProgress/config"
+	"AiProgress/routers"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func server() {
+	var r *gin.Engine
+	r = routers.RouteController()
+	r.Run("127.0.0.1:8080")
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	godotenv.Load() // 加载环境变量文件
+	config.LoadConfig()
+	err := mysql.Init()
+	if err != nil {
+		fmt.Println("数据库初始化失败:", err)
+	}
+	redis.Init()
+	log.Println("redis init success  ")
+	rabbitmq.Init()
+	log.Println("rabbitmq init success  ")
+	server()
 }
